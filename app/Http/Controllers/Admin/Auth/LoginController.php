92eb203cb3dafbcd3c9b10536cea40afb7c238bb
James Ilaki <mr@kjamesy.london>
2020-09-23 11:31:52 +0100

    Add LoginController

---
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+}
