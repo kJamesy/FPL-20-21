5228acde94dc66834edcd82febdef4ec1968e0ed
James Ilaki <mr@kjamesy.london>
2020-09-23 10:35:27 +0100

    Add User and LoginActivity models

---
+++ app/Models/LoginActivity.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LoginActivity extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var array
+     */
+    protected $dates = ['created_at', 'updated_at', 'attempted_at'];
+
+    /**
+     * A LoginActivity belongs to a User
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name', 'last_name', 'username', 'email', 'password', 'active', 'meta',
     ];
 
     /**
@@ -30,6 +30,14 @@ class User extends Authenticatable
     ];
 
     /**
+     * Custom attributes
+     * @var array
+     */
+    protected $appends = [
+        'name', 'is_super_admin', 'is_user', 'last_login', 'penultimate_login',
+    ];
+
+    /**
      * The attributes that should be cast to native types.
      *
      * @var array
@@ -37,4 +45,83 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Validation rules
+     * @var array
+     */
+    public static $rules = [
+        'first_name' => 'required|max:255',
+        'last_name' => 'required|max:255',
+        'username' => 'required|max:255|unique:users',
+        'email' => 'required|email|max:255|unique:users',
+        'password' => 'required|min:6|confirmed',
+    ];
+
+    /**
+     * A user has many Login Activities
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function login_activities()
+    {
+        return $this->hasMany(LoginActivity::class);
+    }
+
+    /**
+     * 'name' accessor
+     * @return string
+     */
+    public function getNameAttribute()
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
+
+    /**
+     * 'is_super_admin' accessor
+     * @return bool
+     */
+    public function getIsSuperAdminAttribute()
+    {
+        $meta = json_decode($this->meta);
+        return $meta && property_exists($meta, 'role') && strtolower($meta->role) === 'super administrator';
+    }
+
+    /**
+     * 'is_user' accessor
+     * @return bool
+     */
+    public function getIsUserAttribute()
+    {
+        $meta = json_decode($this->meta);
+        return $meta && property_exists($meta, 'role') && strtolower($meta->role) === 'user';
+    }
+
+    /**
+     * 'last_login' accessor
+     * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Relations\HasMany|object|null
+     */
+    public function getLastLoginAttribute()
+    {
+        return $this->login_activities()->where('success', 1)->orderBy('created_at', 'DESC')->first();
+    }
+
+    /**
+     * 'penultimate_login' accessor
+     * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Relations\HasMany|object|null
+     */
+    public function getPenultimateLoginAttribute()
+    {
+        return $this->login_activities()->where('success', 1)->orderBy('created_at', 'DESC')->skip(1)->first();
+    }
+
+    /**
+     * Get default user role
+     * Basically, if we have existing users, the default role is user.
+     * @return string
+     */
+    public static function getDefaultRole()
+    {
+        return static::first() ? 'User' : 'Super Administrator';
+    }
+
 }
